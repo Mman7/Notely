52a0d002b0c8ecab5ceb96d057b5ed7bd5bddaec
Eric Man <youzai0427@gmail.com>
2024-05-21 23:43:39 +0800

    ✅implemented notelist prototype

--- lib/src/section/notelist.dart
+++ lib/src/section/notelist.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:syncnote/src/widget/note_preview_item.dart';
+import '../demo_data.dart';
 
 class NoteList extends StatelessWidget {
-  const NoteList({
+  NoteList({
     super.key,
   });
 
@@ -9,9 +11,26 @@ class NoteList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.red,
-      child: const Column(
+      child: Column(
         children: [
           Text('Note List Section'),
+          Expanded(
+            child: Container(
+              color: Colors.lime,
+              child: ListView.builder(
+                  itemCount: demo_data.length,
+                  itemBuilder: (context, index) {
+                    var item = demo_data[index];
+
+                    return NotePreviewItem(
+                        date: item.timeCreated,
+                        title: item.title,
+                        content: item.data,
+                        hadPreviewIMG: item.hadPreviewIMG);
+                  }),
+              // itemBuilder: (context, index) => Text(demo_data[index].data),
+            ),
+          ),
         ],
       ),
     );

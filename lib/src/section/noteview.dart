7c770ff97347aeb26a339d59a2e90348d9ce27ac
Eric Man <youzai0427@gmail.com>
2024-05-21 23:43:07 +0800

    ✅implemented noteview prototype

--- lib/src/section/noteview.dart
+++ lib/src/section/noteview.dart
@@ -1,17 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart';
+
+//TODO mobile if user editing show bottom sticky TOOLBAR
 
 class NoteView extends StatelessWidget {
-  const NoteView({
+  NoteView({
     super.key,
   });
+  final QuillController _controller = QuillController.basic();
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.green,
-      child: const Column(
+      child: Column(
         children: [
-          Text('Edit Note Section'),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.yellow,
+              child: QuillToolbar.simple(
+                configurations: QuillSimpleToolbarConfigurations(
+                  controller: _controller,
+                  sharedConfigurations: const QuillSharedConfigurations(
+                    locale: Locale('en'),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 7,
+            child: Container(
+              color: Colors.purple,
+              child: QuillEditor.basic(
+                configurations: QuillEditorConfigurations(
+                  controller: _controller,
+                  sharedConfigurations: const QuillSharedConfigurations(
+                    locale: Locale('en'),
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );

347ae054e437c6f8c46542f79fb3bddd67659cf8
Eric Man <youzai0427@gmail.com>
2024-05-30 21:41:43 +0800

    ✅implemented objectbox and provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:syncnote/src/provider/app_provider.dart';
 import 'package:syncnote/src/section/menu.dart';
 import 'package:syncnote/src/section/notelist.dart';
 import 'package:syncnote/src/section/noteview.dart';
-
-void main() {
-  runApp(const MainApp());
-}
+import 'package:syncnote/src/utils/myobjectbox.dart';
+import 'myobjectbox.dart';
 
 //TODO use fuzzy to search data
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  objectbox = await ObjectBox.create();
+  runApp(ChangeNotifierProvider(
+    create: (ctx) => AppProvider(),
+    child: const MainApp(),
+  ));
+}
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -18,7 +27,7 @@ class MainApp extends StatelessWidget {
       home: Scaffold(
         body: Container(
           color: Colors.black,
-          child: Row(
+          child: const Row(
             children: [
               Flexible(fit: FlexFit.tight, flex: 2, child: Menu()),
               Flexible(fit: FlexFit.tight, flex: 3, child: NoteList()),

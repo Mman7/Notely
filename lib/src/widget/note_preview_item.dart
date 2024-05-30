ae2ab7c13515b451834ecdb46f8bd263bb959c03
Eric Man <youzai0427@gmail.com>
2024-05-30 21:44:39 +0800

    ✅implemented preview demo

--- lib/src/widget/note_preview_item.dart
+++ lib/src/widget/note_preview_item.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:syncnote/src/provider/app_provider.dart';
 
 class NotePreviewItem extends StatelessWidget {
   const NotePreviewItem(
       {super.key,
+      required this.id,
       required this.date,
       required this.title,
       required this.content,
       required this.hadPreviewIMG});
 
+  final int id;
   final String title;
   final String content;
   final DateTime date;
@@ -18,21 +22,27 @@ class NotePreviewItem extends StatelessWidget {
     //if hadPreviewIMG show pic else leave it blank
     //*tesing
     var pic = hadPreviewIMG
-        ? Icon(Icons.ac_unit_rounded, color: Colors.red)
-        : Icon(Icons.ac_unit_rounded, color: Colors.transparent);
+        ? const Icon(Icons.ac_unit_rounded, color: Colors.red)
+        : const Icon(Icons.ac_unit_rounded, color: Colors.blue);
 
-    return ListTile(
-        title: Text(title),
-        subtitle: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(content),
-            Text(DateFormat('yyyy-MM-dd – kk:mm').format(date))
-          ],
-        ),
-        //*testing
-        trailing: pic
-        // trailing: Image(),
-        );
+    return Material(
+      color: Colors.teal,
+      child: InkWell(
+        onTap: () => context.read<AppProvider>().setNoteSelected(id: id),
+        child: ListTile(
+            title: Text(title),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(content),
+                Text(DateFormat('yyyy-MM-dd – kk:mm').format(date))
+              ],
+            ),
+            //*testing
+            trailing: pic
+            // trailing: Image(),
+            ),
+      ),
+    );
   }
 }

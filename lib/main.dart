9d2664fd710413ac3dba268adcb6191b0232f019
Eric Man <youzai0427@gmail.com>
2024-05-31 22:21:13 +0800

    ➕add screen utils seperate mobile and desktop screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:syncnote/src/provider/app_provider.dart';
-import 'package:syncnote/src/section/menu.dart';
-import 'package:syncnote/src/section/notelist.dart';
-import 'package:syncnote/src/section/noteview.dart';
+import 'package:syncnote/src/screen/desktop.dart';
+import 'package:syncnote/src/screen/mobile.dart';
 import 'package:syncnote/src/utils/myobjectbox.dart';
 import 'myobjectbox.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 //TODO use fuzzy to search data
 
@@ -18,24 +18,47 @@ void main() async {
   ));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          color: Colors.black,
-          child: const Row(
-            children: [
-              Flexible(fit: FlexFit.tight, flex: 2, child: Menu()),
-              Flexible(fit: FlexFit.tight, flex: 3, child: NoteList()),
-              Flexible(flex: 9, fit: FlexFit.tight, child: NoteView())
-            ],
-          ),
-        ),
-      ),
-    );
+    return ScreenUtilInit(
+        designSize: const Size(360, 1080),
+        minTextAdapt: true,
+        builder: (ctx, child) {
+          final screenwidth = ScreenUtil().screenWidth;
+          var isMobile = screenwidth < 490;
+          var isDesktop = screenwidth > 1080;
+          print(screenwidth);
+
+          return MaterialApp(
+            home: Scaffold(
+              // drawer: SideBar(
+              //   sidebarXController: sidebarXController,
+              // ),
+              body: Container(
+                color: Colors.black,
+                child: Builder(
+                  builder: (ctx) {
+                    if (isMobile) return const MobileLayout();
+                    if (isDesktop) return const DesktopLayout();
+                    return Container();
+                  },
+                ),
+              ),
+            ),
+          );
+        });
   }
 }

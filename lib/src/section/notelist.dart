5621dfc9a1a56bd10d7cce7ab27c1c37eb4225b4
Eric Man <youzai0427@gmail.com>
2024-06-05 23:23:23 +0800

    ✅add colour and text

--- lib/src/section/notelist.dart
+++ lib/src/section/notelist.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart';
 import 'package:provider/provider.dart';
 import 'package:syncnote/src/provider/app_provider.dart';
 import 'package:syncnote/src/widget/note_preview_item.dart';
+
+//TODO implement function with sidebar
 
 class NoteList extends StatefulWidget {
   const NoteList({
@@ -26,22 +29,56 @@ class _NoteListState extends State<NoteList> {
     final itemList = context.watch<AppProvider>().noteList;
 
     return Container(
-      color: Colors.red,
+      color: hexToColor('EAF3FC'),
       child: Column(
         children: [
-          const Text('Note List Section'),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+            margin: const EdgeInsets.only(bottom: 8),
+            decoration: BoxDecoration(color: Colors.white, boxShadow: [
+              BoxShadow(
+                color: hexToColor('42526E').withOpacity(0.3),
+                spreadRadius: 1,
+                blurRadius: 30,
+                offset: const Offset(0, 0), // changes position of shadow
+              ),
+            ]),
+            child: Column(
+              children: [
+                const Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'All notes',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Total Notes ${itemList.length}',
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16,
+                      ),
+                    ))
+              ],
+            ),
+          ),
           Expanded(
             child: Container(
-              color: Colors.lime,
+              padding: const EdgeInsets.all(2),
               child: ListView.builder(
                   itemCount: itemList.length,
                   itemBuilder: (context, index) {
-                    return NotePreviewItem(
-                        id: itemList[index].id,
-                        date: itemList[index].dateCreated,
-                        title: itemList[index].title,
-                        content: itemList[index].previewContent,
-                        hadPreviewIMG: itemList[index].includePic);
+                    return Container(
+                      margin: const EdgeInsets.symmetric(vertical: 2),
+                      child: NotePreviewItem(
+                          id: itemList[index].id,
+                          date: itemList[index].dateCreated,
+                          title: itemList[index].title,
+                          content: itemList[index].previewContent,
+                          hadPreviewIMG: itemList[index].includePic),
+                    );
                   }),
               // itemBuilder: (context, index) => Text(demo_data[index].data),
             ),

b666f8ad4e3a02502191a1d3c6d474cdf000e1d5
Eric Man <youzai0427@gmail.com>
2024-06-07 23:16:59 +0800

    ✅update style

--- lib/src/widget/note_preview_item.dart
+++ lib/src/widget/note_preview_item.dart
@@ -3,6 +3,8 @@ import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:syncnote/src/provider/app_provider.dart';
 
+//TODO hold press drop down menu
+
 class NotePreviewItem extends StatelessWidget {
   const NotePreviewItem(
       {super.key,
@@ -17,8 +19,12 @@ class NotePreviewItem extends StatelessWidget {
   final String content;
   final DateTime date;
   final bool hadPreviewIMG;
+
   @override
   Widget build(BuildContext context) {
+    var isSelected = id == context.read<AppProvider>().noteSelected?.id;
+
+    //TODO preview pic function
     //if hadPreviewIMG show pic else leave it blank
     //*tesing
     var pic = hadPreviewIMG
@@ -26,15 +32,25 @@ class NotePreviewItem extends StatelessWidget {
         : const Icon(Icons.ac_unit_rounded, color: Colors.blue);
 
     return Material(
-      color: Colors.teal,
+      borderRadius: const BorderRadius.all(Radius.circular(7.5)),
+      color: isSelected ? Colors.white : Colors.white60,
+      // color: Colors.white,
       child: InkWell(
+        hoverColor: Colors.white,
         onTap: () => context.read<AppProvider>().setNoteSelected(id: id),
         child: ListTile(
-            title: Text(title),
+            title: Text(
+              title,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
             subtitle: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(content),
+                Text(
+                  content,
+                  style: const TextStyle(
+                      color: Colors.black54, fontWeight: FontWeight.w400),
+                ),
                 Text(DateFormat('yyyy-MM-dd – kk:mm').format(date))
               ],
             ),

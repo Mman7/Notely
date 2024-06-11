42b62eb504c8e867fb34d3cd1560d5039474954f
Eric Man <youzai0427@gmail.com>
2024-06-11 21:45:25 +0800

    ✅add setlist

--- lib/src/provider/app_provider.dart
+++ lib/src/provider/app_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:syncnote/myobjectbox.dart';
+import 'package:syncnote/objectbox.g.dart';
 import 'package:syncnote/src/model/note_model.dart';
 
 class AppProvider extends ChangeNotifier {
@@ -7,9 +8,30 @@ class AppProvider extends ChangeNotifier {
   List noteList = [];
   Note? noteSelected;
   bool searchMode = false;
+  String listMode = 'allnotes';
+  bool isSidebarExtended = false;
+
+  changeSidebarExtended() {
+    isSidebarExtended = !isSidebarExtended;
+    notifyListeners();
+  }
 
   setSearchMode({value}) {
     searchMode = value;
+    notifyListeners();
+  }
+
+  /// get special data
+  getSpData() {
+    final query = noteBox.query(Note_.isBookmark.equals(true)).build();
+    final data = query.find();
+
+    print(data);
+  }
+
+  ///   value = [allnotes,bookmarks,notebooks or tags]
+  setListMode({value}) {
+    listMode = value;
     notifyListeners();
   }
 
@@ -29,6 +51,7 @@ class AppProvider extends ChangeNotifier {
   intializeData() {
     var data = noteBox.getAll();
     noteList = data;
+
     notifyListeners();
   }
 

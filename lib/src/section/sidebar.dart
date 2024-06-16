0dd2acfe94c7996b06ddf751d9ac3540a6cdeed4
Eric Man <youzai0427@gmail.com>
2024-06-16 16:18:38 +0800

    ✅Add Expansion List Button

--- lib/src/section/sidebar.dart
+++ lib/src/section/sidebar.dart
@@ -1,10 +1,13 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'package:syncnote/src/provider/app_provider.dart';
+import 'package:syncnote/src/widget/custom_expansion_list.dart';
 import 'package:syncnote/src/widget/custom_text_button.dart';
 
 //*do this first TODO: implement function with sidebar
-//TODO layout problem
 
 class SideBar extends StatefulWidget {
   const SideBar({super.key});
@@ -14,11 +17,32 @@ class SideBar extends StatefulWidget {
 }
 
 class _SideBarState extends State<SideBar> {
-  MaterialStatesController statesController = MaterialStatesController();
+  bool noteBookisExpanded = false;
+  bool tagsIsExpanded = false;
+  TextEditingController textEditController = TextEditingController();
+
+  @override
+  void dispose() {
+    textEditController.dispose();
+    super.dispose();
+  }
+
+  setNoteBookExpanded({required bool value}) {
+    setState(() {
+      noteBookisExpanded = value;
+    });
+  }
+
+  setTagsExpanded({required bool value}) {
+    setState(() {
+      tagsIsExpanded = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     var isExtended = context.watch<AppProvider>().isSidebarExtended;
+    var appProvider = context.read<AppProvider>();
 
     openIcon() {
       return isExtended
@@ -26,85 +50,87 @@ class _SideBarState extends State<SideBar> {
           : Icons.keyboard_arrow_right_sharp;
     }
 
-    var appProvider = context.read<AppProvider>();
-
     return Container(
       width: double.infinity,
-      decoration:
-          const BoxDecoration(color: Colors.white, boxShadow: [BoxShadow()]),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+      ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
+        // crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CustomTextButton(
-                  icon: Icons.search,
-                  text: ' Search',
-                  showText: isExtended,
-                  onPressed: () {
-                    appProvider.setSearchMode(value: true);
-                  },
-                ),
-                CustomTextButton(
-                  icon: Icons.add_circle_rounded,
-                  text: ' New Note',
-                  showText: isExtended,
-                  onPressed: () {
-                    appProvider.setSearchMode(value: false);
-                  },
-                ),
-                CustomTextButton(
-                  icon: Icons.book,
-                  text: ' All Notes',
-                  showText: isExtended,
-                  onPressed: () {
-                    appProvider.setSearchMode(value: false);
-                    appProvider.setListMode(value: 'allnotes');
-                  },
-                ),
-                CustomTextButton(
-                  icon: Icons.star,
-                  text: ' Bookmarks',
-                  showText: isExtended,
-                  onPressed: () {
-                    appProvider.setListMode(value: 'bookmarks');
-                  },
-                ),
-                //TODO: notesbooks menu with create notebooks button under it
-                CustomTextButton(
-                  icon: Icons.menu_book,
-                  text: ' Notebooks',
-                  showText: isExtended,
-                  onPressed: () {
-                    appProvider.setSearchMode(value: false);
-                    appProvider.setListMode(value: 'notebooks');
-                  },
-                ),
-                //TODO: tags menu with create tags button under it
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomTextButton(
+                icon: Icons.search,
+                text: ' Search',
+                onPressed: () {
+                  appProvider.setSearchMode(value: true);
+                },
+              ),
+              CustomTextButton(
+                bgColor: Colors.redAccent,
+                btnColor: Colors.white,
+                textColor: Colors.white,
+                margin: const EdgeInsets.all(7.5),
+                borderRadiusValue: BorderRadius.circular(8.5),
+                icon: Icons.add_circle_rounded,
+                text: ' New Note',
+                onPressed: () {
+                  appProvider.setSearchMode(value: false);
+                  appProvider.setNoteSelected(id: 0);
+                },
+              ),
+              CustomTextButton(
+                icon: Icons.book,
+                text: ' All Notes',
+                onPressed: () {
+                  appProvider.setSearchMode(value: false);
+                  appProvider.setListMode(value: 'allnotes');
+                },
+              ),
+              CustomTextButton(
+                icon: Icons.bookmark,
+                text: ' Bookmarks',
+                onPressed: () {
+                  appProvider.setListMode(value: 'bookmarks');
+                },
+              ),
 
-                CustomTextButton(
-                  icon: Icons.bookmark,
-                  text: ' Tags',
-                  showText: isExtended,
-                  onPressed: () {
-                    appProvider.setSearchMode(value: false);
-                    appProvider.setListMode(value: 'tags');
-                  },
-                ),
-              ],
-            ),
+              //add colour
+              CustomExpansionList(
+                setToggle: () =>
+                    setNoteBookExpanded(value: !noteBookisExpanded),
+                list: [],
+                createBtnCallBack: () => showD(context),
+                isExpanded: noteBookisExpanded,
+                icon: Icons.menu_book,
+                text: ' Notebooks',
+              ),
+
+              CustomExpansionList(
+                list: [],
+                createBtnCallBack: () => showD(context),
+                setToggle: () {
+                  setTagsExpanded(value: !tagsIsExpanded);
+                },
+                isExpanded: tagsIsExpanded,
+                icon: Icons.menu_book,
+                text: ' Tags',
+              ),
+            ],
           ),
+
+          // close expanded button
           SizedBox(
             width: double.infinity,
             child: Material(
                 child: InkWell(
                     onTap: () {
                       context.read<AppProvider>().changeSidebarExtended();
+                      setNoteBookExpanded(value: false);
+                      setTagsExpanded(value: false);
                     },
                     child: Icon(openIcon()))),
           )
@@ -112,95 +138,48 @@ class _SideBarState extends State<SideBar> {
       ),
     );
   }
+
+  void showD(BuildContext context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) => SimpleDialog(children: [
+              Container(
+                // TODO :content fit
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+                width: ScreenUtil().screenWidth / 2,
+                height: ScreenUtil().screenHeight / 2,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextField(
+                      controller: textEditController,
+                      decoration: const InputDecoration(
+                        hintText: 'Title',
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        TextButton.icon(
+                            label: const Text('Create'),
+                            onPressed: () {
+                              print(textEditController.text);
+                            },
+                            icon: const Icon(Icons.check_circle)),
+                        TextButton.icon(
+                            label: const Text('Cancel'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                              textEditController.clear();
+                            },
+                            icon: const Icon(Icons.highlight_remove)),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ]));
+  }
 }
-
-// class SideBar extends StatelessWidget {
-//   const SideBar({
-//     super.key,
-//     required this.sidebarXController,
-//   });
-
-//   final SidebarXController sidebarXController;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final appProvider = context.read<AppProvider>();
-//     return SidebarX(
-//       controller: sidebarXController,
-//       theme: SidebarXTheme(
-//         decoration: BoxDecoration(
-//           gradient: LinearGradient(
-//               begin: Alignment.topLeft,
-//               end: Alignment.bottomRight,
-//               colors: <Color>[hexToColor('0064C1'), hexToColor('002547')]),
-//           // color: hexToColor('001E3B'),
-//         ),
-//         selectedItemDecoration: const BoxDecoration(
-//             color: Colors.white10,
-//             borderRadius: BorderRadius.all(Radius.circular(7))),
-//         textStyle:
-//             const TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
-//         iconTheme: const IconThemeData(color: Colors.white),
-//         hoverTextStyle:
-//             const TextStyle(color: Colors.blue, fontWeight: FontWeight.w700),
-//         selectedTextStyle:
-//             const TextStyle(color: Colors.blue, fontWeight: FontWeight.w700),
-//         selectedIconTheme: const IconThemeData(
-//           color: Colors.blue,
-//         ),
-//       ),
-//       separatorBuilder: (context, index) {
-//         return const Gap(10);
-//       },
-//       items: [
-//         SidebarXItem(
-//             label: ' Search',
-//             icon: Icons.search,
-//             onTap: () {
-//               appProvider.setSearchMode(value: true);
-//             }),
-//             iconBuilder: (selected, hovered) {
-//         SidebarXItem(
-//               return const Icon(
-//                 Icons.add_circle_rounded,
-//                 color: Colors.orange,
-//               );
-//             },
-//             label: ' New Note',
-//             selectable: false,
-//             onTap: () {
-//               appProvider.setSearchMode(value: false);
-//               appProvider.setNoteSelected(id: 0);
-//             }),
-//         SidebarXItem(
-//             onTap: () {
-//               appProvider.setSearchMode(value: false);
-//             },
-//             icon: Icons.book,
-//             label: ' All Notes'),
-//         SidebarXItem(
-//             onTap: () {
-//               appProvider.setSearchMode(value: false);
-//               appProvider.setListMode(value: 'bookmarks');
-//               appProvider.getSpData();
-//             },
-//             icon: Icons.star,
-//             label: ' Bookmarks'),
-//         SidebarXItem(
-//             onTap: () {
-//               appProvider.setSearchMode(value: false);
-//               appProvider.setListMode(value: 'notebooks');
-//             },
-//             icon: Icons.menu_book,
-//             label: ' Notebooks'),
-//         SidebarXItem(
-//             onTap: () {
-//               appProvider.setSearchMode(value: false);
-//               appProvider.setListMode(value: 'tags');
-//             },
-//             icon: Icons.bookmark,
-//             label: ' Tags'),
-//       ],
-//     );
-//   }
-// }

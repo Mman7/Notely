a0b48cafc3cbd2202f9afaec4e3e60d882f9216c
Eric Man <youzai0427@gmail.com>
2025-05-12 19:59:53 +0800

    add more color

--- lib/src/theme.dart
+++ lib/src/theme.dart
@@ -2,7 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 
 final myTheme = ThemeData(
+    textTheme: TextTheme(
+      bodyLarge: TextStyle(color: Colors.white),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      fillColor: Colors.orange,
+      filled: true,
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: Colors.white,
+      selectedItemColor: hexToColor('#504099'),
+      unselectedItemColor: hexToColor('#E9E6FD'),
+    ),
     colorScheme: ColorScheme.light().copyWith(
-  primary: hexToColor('#50409A'),
-  secondary: hexToColor('#313866'),
-));
+        primary: hexToColor('#504099'),
+        secondary: hexToColor('#E9E6FD'),
+        surface: hexToColor('#F9F8FD')));

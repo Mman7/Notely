fec57c387fec1fab4103c8e10caa2d9884484670
Eric Man <youzai0427@gmail.com>
2025-05-12 20:01:54 +0800

    update

--- lib/src/screen/desktop.dart
+++ lib/src/screen/desktop.dart
@@ -1,10 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:provider/provider.dart';
-import 'package:syncnote/src/provider/app_provider.dart';
-import 'package:syncnote/src/section/notelist.dart';
-import 'package:syncnote/src/section/noteview.dart';
+import 'package:syncnote/src/section/note_list.dart';
 import 'package:syncnote/src/section/sidebar.dart';
 
 class DesktopLayout extends StatefulWidget {
@@ -16,34 +11,33 @@ class DesktopLayout extends StatefulWidget {
 
 class _DesktopLayoutState extends State<DesktopLayout>
     with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation _animation;
-
   @override
   void initState() {
     super.initState();
-    _animationController = AnimationController(
-        duration: const Duration(milliseconds: 200), vsync: this);
-    _animation = IntTween(begin: 80, end: 200).animate(
-        CurvedAnimation(parent: _animationController, curve: Curves.easeIn));
-    _animation.addListener(() => setState(() {}));
   }
 
   @override
   Widget build(BuildContext context) {
-    if (context.watch<AppProvider>().isSidebarExtended) {
-      _animationController.forward();
-    } else {
-      _animationController.reverse();
-    }
     return Row(
       children: [
         Flexible(
-          flex: _animation.value,
-          child: SideBar(aniContoller: _animationController),
+          flex: 2,
+          child: SideBar(),
         ),
-        const Flexible(flex: 500, child: NoteList()),
-        const Flexible(flex: 1000, child: NoteView())
+        Flexible(
+            flex: 9,
+            child:
+                //  Container(
+                //   color: Theme.of(context).colorScheme.surface,
+                //   child: Column(
+                //     children: [
+                //       Expanded(flex: 1, child: Header()),
+                //       Expanded(flex: 8, child: FolderListView())
+                //     ],
+                //   ),
+                // )
+                NoteList())
+        // const Flexible(flex: 5, child: NoteView())
       ],
     );
   }
--- lib/src/screen/mobile.dart
+++ lib/src/screen/mobile.dart
@@ -1,31 +1,26 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:syncnote/src/modules/socket.dart';
+import 'package:syncnote/src/section/folder_list_view.dart';
+import 'package:syncnote/src/section/note_list.dart';
 
 class MobileLayout extends StatelessWidget {
   const MobileLayout({super.key});
-//TODO implement mobile page
-//TODO buld mobile apk
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-            color: Colors.orange,
-            child: TextButton(
-                onPressed: () {
-                  SocketService().startSocket();
-                },
-                child: Text('start server'))),
-        Container(
-            color: Colors.orange,
-            child: TextButton(
-                onPressed: () {
-                  SocketService().sendMsg();
-                },
-                child: Text('Connect server'))),
-      ],
+    // Navigate to NoteList page (triggered by user interaction)
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => NoteList()),
+      );
+    });
+    return Container(
+      color: Theme.of(context).colorScheme.surface,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [FolderListView()],
+        ),
+      ),
     );
   }
 }

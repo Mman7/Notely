37b03ddadf3d0bde90b01e043e4b7c8e281bbf38
Eric Man <youzai0427@gmail.com>
2025-05-20 02:13:54 +0800

    a working editor with save and update function

--- lib/src/section/editor.dart
+++ lib/src/section/editor.dart
@@ -1,23 +1,36 @@
+import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quill/flutter_quill.dart';
+import 'package:flutter_quill/quill_delta.dart';
+import 'package:provider/provider.dart';
+import 'package:syncnote/src/model/note_model.dart';
+import 'package:syncnote/src/modules/local_database.dart';
+import 'package:syncnote/src/provider/app_provider.dart';
+import 'package:uuid/uuid.dart';
 
 class Editor extends StatefulWidget {
   const Editor({
     super.key,
     required this.title,
     required this.content,
+    this.uuid,
+    this.id,
   });
   final String title;
   final String content;
+  final String? uuid;
+  final int? id;
   @override
   State createState() => _EditorState();
 }
 
+/// TODO Add bottom sticky bar for mobile
 class _EditorState extends State<Editor> {
   final QuillController _controller = QuillController.basic();
   final _titleController = TextEditingController();
+  bool isChanged = true;
 
   @override
   void dispose() {
@@ -29,36 +42,59 @@ class _EditorState extends State<Editor> {
   @override
   void initState() {
     _titleController.text = widget.title;
-    var originalContent = widget.content;
-    // test data
-    List<Map<String, dynamic>> data = [
-      {
-        "insert": '$originalContent\n',
-      }
-    ];
-    _controller.document = Document.fromJson(data);
+    String originalContent = widget.content;
+    if (originalContent.isEmpty) {
+      return;
+    }
+    var json = jsonDecode(originalContent);
+
+    _controller.document = Document.fromJson(json);
 
     _controller.addListener(() {
-      var aaa = _controller.document.toDelta().toJson();
-      print(mapEquals(aaa[0], data[0]));
+      List<Map<String, dynamic>> aaa = _controller.document.toDelta().toJson();
+      // setState(() => isChanged = !isContentChanged(aaa, data));
     });
     super.initState();
   }
 
+  isNewNote() => widget.content.isEmpty;
   // Check if the content has changed
-  bool isContentChanged(
-      List<Map<String, dynamic>> value1, List<Map<String, dynamic>> value2) {
-    if (value1.length != value2.length) {
-      return true;
-    }
-    return mapEquals(value1[0], value2[0]);
-  }
+  bool isContentChanged(List<Map<String, dynamic>> value1,
+          List<Map<String, dynamic>> value2) =>
+      mapEquals(value1[0], value2[0]);
 
   @override
   Widget build(BuildContext context) {
+    AppProvider provider = context.read<AppProvider>();
     return Scaffold(
       appBar: AppBar(
-        title: const Text('MeloEditor'),
+        actions: [
+          isChanged
+              ? IconButton(
+                  onPressed: () {
+                    // Save the content
+                    String title = _titleController.text;
+                    Delta content = _controller.document.toDelta();
+                    String preview = _controller.document.toPlainText();
+                    var json = jsonEncode(content);
+                    Database().saveNote(
+                      note: Note(
+                        id: widget.id ?? 0,
+                        title: title,
+                        content: json,
+                        dateCreated: DateTime.now(),
+                        uuid: widget.uuid ?? Uuid().v4(),
+                        previewContent: preview,
+                      ),
+                    );
+
+                    provider.refresh();
+                  },
+                  icon: const Icon(Icons.save),
+                )
+              : Container()
+        ],
+        title: const Text('MelosEditor'),
       ),
       body: Column(
         children: [

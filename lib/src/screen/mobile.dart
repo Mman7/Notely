a22baed3f8e5a0df742f6d38030c05b0a323ab3e
Eric Man <youzai0427@gmail.com>
2025-05-20 02:13:19 +0800

    remove useless code

--- lib/src/screen/mobile.dart
+++ lib/src/screen/mobile.dart
@@ -1,19 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:syncnote/src/section/folder_list_view.dart';
-import 'package:syncnote/src/section/note_list.dart';
 
 class MobileLayout extends StatelessWidget {
   const MobileLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Navigate to NoteList page (triggered by user interaction)
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => NoteList()),
-      );
-    });
     return Container(
       color: Theme.of(context).colorScheme.surface,
       child: SingleChildScrollView(

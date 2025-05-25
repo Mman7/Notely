99caf92ee36c2df417b5920d3ae99d6d90d1f6ec
Eric Man <youzai0427@gmail.com>
2025-05-25 16:15:04 +0800

    Refactor Editor widget and add save delete function

--- lib/src/section/editor.dart
+++ lib/src/section/editor.dart
@@ -12,7 +12,7 @@ import 'package:toastification/toastification.dart';
 import 'package:uuid/uuid.dart';
 
 class Editor extends StatefulWidget {
-  const Editor({
+  Editor({
     super.key,
     required this.title,
     required this.content,
@@ -20,21 +20,19 @@ class Editor extends StatefulWidget {
     this.id,
     required this.isNew,
   });
-  final String title;
-  final String content;
-  final String? uuid;
-  final int? id;
-  final bool isNew;
+  String title;
+  String content;
+  String? uuid;
+  int? id;
+  bool isNew;
   @override
   State createState() => _EditorState();
 }
 
-// TODO when user save set this.uuid,title,content
-// TODO add delete note
-
 class _EditorState extends State<Editor> {
   final QuillController _controller = QuillController.basic();
   final _titleController = TextEditingController();
+  FocusNode editorFocusNode = FocusNode();
   ValueNotifier<bool> isChanged = ValueNotifier(false);
   bool isEditing = false;
 
@@ -49,69 +47,77 @@ class _EditorState extends State<Editor> {
   void initState() {
     _titleController.text = widget.title;
     String originalContent = widget.content;
-    if (originalContent.isEmpty) {
-      return;
-    }
-    // intialize content
-    List json = jsonDecode(originalContent);
-    _controller.document = Document.fromJson(json);
 
-    // isEditing
+    if (!widget.isNew) {
+      // load content
+      List json = jsonDecode(originalContent);
+      _controller.document = Document.fromJson(json);
+    }
 
     // Check if title has changed
-    //TODO fix if content is changed but title didnt, isChange.value always true
     _titleController.addListener(() {
+      if (isChanged.value == true) return;
       if (_titleController.text != widget.title) isChanged.value = true;
       if (_titleController.text == widget.title) isChanged.value = false;
     });
-
     // Check if the content has changed
     _controller.document.changes.listen((event) {
       final change = event.change;
-      if (change.isNotEmpty) {
-        isChanged.value = true;
-      } else {
-        isChanged.value = false;
-      }
+      if (change.isNotEmpty) isChanged.value = true;
+      if (change.isEmpty) isChanged.value = false;
     });
-
     super.initState();
   }
 
-  FocusNode editorFocusNode = FocusNode();
+  void saveContent() {
+    String title = _titleController.text;
+    Delta content = _controller.document.toDelta();
+    String preview = _controller.document.toPlainText();
+    var json = jsonEncode(content);
+    Database().saveNote(
+      note: Note(
+        id: widget.id ?? 0,
+        title: title,
+        content: json,
+        dateCreated: DateTime.now(),
+        uuid: widget.uuid ?? Uuid().v4(),
+        previewContent: preview,
+      ),
+    );
+  }
+
+  /// Updates the widget's properties with the latest note data from the provider.
+  void refreshWhenSave() {
+    Note note = context.read<AppProvider>().noteList.last;
+    widget.title = note.title;
+    widget.content = note.content;
+    widget.id = note.id;
+    widget.isNew = false;
+    widget.uuid = note.uuid;
+    isChanged.value = false;
+    setState(() => isEditing = false);
+  }
 
   @override
   Widget build(BuildContext context) {
     AppProvider provider = context.read<AppProvider>();
     DeviceType deviceType = provider.getDeviceType();
+    // Check is editing
     editorFocusNode.addListener(() {
-      if (isChanged.value) {
-        setState(() {
-          isEditing = false;
-        });
-      } else {
-        setState(() {
-          isEditing = true;
-        });
-      }
+      if (editorFocusNode.hasFocus) setState(() => isEditing = true);
     });
 
-    Widget toolbar = Container(
-        color: Theme.of(context).colorScheme.primary,
-        width: double.infinity,
-        child: QuillToolbar.simple(
-            controller: _controller,
-            configurations: QuillSimpleToolbarConfigurations(
-              sharedConfigurations: const QuillSharedConfigurations(
-                locale: Locale('en'),
-              ),
-            )));
-
     Widget mobileLayoutToolbar = Column(
-      children: [Spacer(), toolbar],
+      children: [Spacer(), Toolbar(controller: _controller)],
     );
+
+    Widget titleTextField = TitleWidget(titleController: _titleController);
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.white,
+        surfaceTintColor: Colors.white,
+        elevation: 6,
+        shadowColor: Colors.grey.withAlpha(128),
         actions: [
           ValueListenableBuilder(
             valueListenable: isChanged,
@@ -119,20 +125,7 @@ class _EditorState extends State<Editor> {
                 ? IconButton(
                     onPressed: () {
                       // Save the content
-                      String title = _titleController.text;
-                      Delta content = _controller.document.toDelta();
-                      String preview = _controller.document.toPlainText();
-                      var json = jsonEncode(content);
-                      Database().saveNote(
-                        note: Note(
-                          id: widget.id ?? 0,
-                          title: title,
-                          content: json,
-                          dateCreated: DateTime.now(),
-                          uuid: widget.uuid ?? Uuid().v4(),
-                          previewContent: preview,
-                        ),
-                      );
+                      saveContent();
                       toastification.show(
                         style: ToastificationStyle.fillColored,
                         primaryColor: Colors.lightGreen,
@@ -140,42 +133,72 @@ class _EditorState extends State<Editor> {
                         context:
                             context, // optional if you use ToastificationWrapper
                         title: Text('Your note has been saved'),
-                        autoCloseDuration: const Duration(seconds: 3),
+                        autoCloseDuration: const Duration(seconds: 2),
                       );
                       provider.refresh();
-                      isChanged.value = false;
+                      refreshWhenSave();
                     },
                     icon: const Icon(Icons.done),
                   )
                 : Container(),
-          )
+          ),
+          !widget.isNew
+              ? PopupMenuButton<int>(
+                  icon: Icon(Icons.more_vert),
+                  onSelected: (value) {
+                    // Handle menu item selection here
+                    // Delete Note
+                    if (value == 1) {
+                      // Prevent widget is new
+                      if (widget.id == null) return;
+
+                      Database().removeNote(id: widget.id);
+                      context.read<AppProvider>().refresh();
+                      toastification.show(
+                        style: ToastificationStyle.minimal,
+                        primaryColor: Colors.lightGreen,
+                        icon: Icon(Icons.done),
+                        context:
+                            context, // optional if you use ToastificationWrapper
+                        title: Text('Your note has been deleted'),
+                        autoCloseDuration: const Duration(seconds: 2),
+                      );
+                      Navigator.of(context).pop();
+                    }
+                  },
+                  itemBuilder: (context) => [
+                    PopupMenuItem(
+                      value: 1,
+                      child: Row(
+                        children: [
+                          Icon(Icons.delete, color: Colors.black),
+                          SizedBox(width: 8),
+                          Text('Delete Note'),
+                        ],
+                      ),
+                    ),
+                    // Add more menu items if needed
+                  ],
+                )
+              : Container()
         ],
         title: const Text('MeloEditor'),
       ),
       body: Container(
-        color: Colors.red,
+        color: Colors.white,
         child: Stack(
           clipBehavior: Clip.none,
           children: [
             Column(
               children: [
-                deviceType == DeviceType.windows ? toolbar : Container(),
-                TextField(
-                  style: TextStyle(
-                      color: Theme.of(context).colorScheme.onSurface,
-                      fontWeight: FontWeight.w500),
-                  controller: _titleController,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    fillColor: Theme.of(context).colorScheme.secondary,
-                    labelText: 'Title',
-                    hintText: 'Title',
-                  ),
-                ),
+                deviceType == DeviceType.windows
+                    ? Toolbar(controller: _controller)
+                    : Container(),
+                titleTextField,
                 Expanded(
                   child: Container(
                     padding: const EdgeInsets.all(8),
-                    color: Theme.of(context).colorScheme.secondary,
+                    color: hexToColor('#FFFFFF'),
                     child: QuillEditor.basic(
                       focusNode: editorFocusNode,
                       configurations: QuillEditorConfigurations(
@@ -190,7 +213,6 @@ class _EditorState extends State<Editor> {
                 ),
               ],
             ),
-            //TODO style the toolbar
             // Toolbar
             deviceType != DeviceType.windows
                 ? isEditing
@@ -199,6 +221,77 @@ class _EditorState extends State<Editor> {
                 : Container()
           ],
         ),
+      ),
+    );
+  }
+}
+
+class Toolbar extends StatelessWidget {
+  const Toolbar({
+    super.key,
+    required QuillController controller,
+  }) : _controller = controller;
+
+  final QuillController _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.symmetric(vertical: 10),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey,
+                blurRadius: 20,
+                offset: Offset(0, 2),
+                spreadRadius: 3),
+          ],
+        ),
+        child: QuillToolbar.simple(
+            controller: _controller,
+            configurations: QuillSimpleToolbarConfigurations(
+              buttonOptions: QuillSimpleToolbarButtonOptions(
+                  base: QuillToolbarBaseButtonOptions(
+                      iconTheme: QuillIconTheme(
+                          iconButtonSelectedData: IconButtonData(
+                              style: ButtonStyle(
+                                  backgroundColor: WidgetStatePropertyAll(
+                                      Theme.of(context)
+                                          .colorScheme
+                                          .primary
+                                          .withAlpha(200))))))),
+              sharedConfigurations: const QuillSharedConfigurations(
+                locale: Locale('en'),
+              ),
+            )));
+  }
+}
+
+class TitleWidget extends StatelessWidget {
+  const TitleWidget({
+    super.key,
+    required TextEditingController titleController,
+  }) : _titleController = titleController;
+
+  final TextEditingController _titleController;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      style: TextStyle(
+          color: Theme.of(context).colorScheme.onSurface,
+          fontSize: 30,
+          fontWeight: FontWeight.w500),
+      controller: _titleController,
+      decoration: InputDecoration(
+        hintStyle: TextStyle(color: Colors.grey),
+        contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+        border: InputBorder.none,
+        fillColor: hexToColor('#FFFFFF'),
+        hintText: 'Title',
+        hoverColor: Colors.transparent,
       ),
     );
   }

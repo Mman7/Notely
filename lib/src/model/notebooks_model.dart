3b9a0f605d1a53d38ac8f5d5fc285bdd760c1c44
Eric Man <youzai0427@gmail.com>
2025-05-28 23:16:23 +0800

    remove description

--- lib/src/model/notebooks_model.dart
+++ lib/src/model/notebooks_model.dart
@@ -1,10 +1,13 @@
 import 'package:objectbox/objectbox.dart';
 
+//TODO create
 @Entity()
 class Notebook {
-  Notebook({this.id = 0, required this.title, this.description});
+  Notebook({
+    this.id = 0,
+    required this.title,
+  });
   @Id()
   int id;
   String title;
-  String? description;
 }

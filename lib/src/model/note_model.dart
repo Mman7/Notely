22bf46ddb14c6006aa650919b1257478ae2d6600
Eric Man <youzai0427@gmail.com>
2025-05-28 23:15:51 +0800

    remove dead code

--- lib/src/model/note_model.dart
+++ lib/src/model/note_model.dart
@@ -2,25 +2,24 @@ import 'package:objectbox/objectbox.dart';
 
 @Entity()
 class Note {
-  Note(
-      {this.id = 0,
-      required this.uuid,
-      required this.title,
-      required this.content,
-      required this.previewContent,
-      this.notebook,
-      this.isBookmark = false,
-      this.dateCreated,
-      this.lastestModified,
-      this.includePic = false});
+  Note({
+    this.id = 0,
+    required this.uuid,
+    required this.title,
+    required this.content,
+    required this.previewContent,
+    this.notebook,
+    this.isBookmark = false,
+    this.dateCreated,
+    this.lastestModified,
+  });
   @Id()
   int id;
   String uuid;
   String previewContent;
-  String title;
   String content;
+  String title;
   bool? isBookmark;
-  bool? includePic;
   DateTime? dateCreated;
   List<String>? notebook;
   DateTime? lastestModified;

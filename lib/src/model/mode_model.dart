b5129ed7261b37d3d10a5e921481a63da74675fd
Eric Man <youzai0427@gmail.com>
2025-05-29 21:04:13 +0800

    delete

--- lib/src/model/mode_model.dart
+++
@@ -1,7 +0,0 @@
-class Mode {
-  static const String bookmarks = 'Bookmarks';
-  static const String allnotes = 'All Notes';
-  static const String tags = 'Tags';
-  static const String search = 'Search';
-  static const String noteBook = 'Note Book';
-}

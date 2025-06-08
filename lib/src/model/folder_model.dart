4e68dda767182bddef1c635a7131f91962b4ca46
Eric Man <youzai0427@gmail.com>
2025-06-08 21:24:13 +0800

    add refresh note

--- lib/src/model/folder_model.dart
+++ lib/src/model/folder_model.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:objectbox/objectbox.dart';
+import 'package:syncnote/src/model/note_model.dart';
+import 'package:syncnote/src/modules/local_database.dart';
 
 @Entity()
 class FolderModel {
@@ -10,7 +12,21 @@ class FolderModel {
   String title;
   // List covert into string
   String? noteInclude;
-  List<int> getConvertNoteInclude() {
+
+  List<int> getConvertNoteIncluded() {
     return List<int>.from(jsonDecode(noteInclude ?? '[]'));
+  }
+
+  /// Filter out [null]
+  void refreshNoteIncluded() {
+    Database database = Database();
+    List result = database.getManyNote(list: getConvertNoteIncluded());
+    List newList = [];
+    for (Note? e in result) {
+      if (e != null) newList.add(e.id);
+    }
+    String json = jsonEncode(newList);
+    noteInclude = json;
+    Database().updateFolder(folder: this);
   }
 }

3930cdc680cc4a47c966acb6b1cb9d576db43689
Eric Man <youzai0427@gmail.com>
2025-06-16 00:27:17 +0800

    remove property folder and method

--- lib/src/model/note_model.dart
+++ lib/src/model/note_model.dart
@@ -12,7 +12,6 @@ class Note {
     required this.title,
     required this.content,
     required this.previewContent,
-    this.folder,
     this.isBookmark = false,
     this.dateCreated,
     this.lastestModified,
@@ -25,29 +24,7 @@ class Note {
   String title;
   bool? isBookmark;
   DateTime? dateCreated;
-  String? folder;
   DateTime? lastestModified;
-  List get getFolderList => _getFolderIncluded();
-  List<int> _getFolderIncluded() {
-    return List<int>.from(jsonDecode(folder ?? '[]'));
-  }
-
-  addFolder({required int folderId}) {
-    List newList = List.from(getFolderList);
-    if (newList.contains(folderId)) {
-      return; // Folder already included
-    }
-    newList.add(folderId);
-    folder = jsonEncode(newList);
-    Database().updateNote(note: this);
-  }
-
-  removeFolder({required int folderId}) {
-    List<int> folderList = _getFolderIncluded();
-    folderList.remove(folderId);
-    folder = jsonEncode(folderList);
-    Database().updateNote(note: this);
-  }
 
   factory Note.newNote() {
     return Note(

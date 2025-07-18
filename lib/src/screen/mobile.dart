e8cab46344b1615be1d85545a4d0533aed136952
EricMan <youzai0427@gmail.com>
2025-07-18 18:43:07 +0800

    fix notelist back btn crash app

--- lib/src/screen/mobile.dart
+++ lib/src/screen/mobile.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:melonote/src/model/note_model.dart';
+import 'package:melonote/src/provider/app_provider.dart';
 import 'package:melonote/src/section/editor.dart';
 import 'package:melonote/src/section/folder_list_view.dart';
 import 'package:melonote/src/section/note_list.dart';
 import 'package:melonote/src/section/settings_page.dart';
 import 'package:melonote/src/section/transfer_page.dart';
+import 'package:provider/provider.dart';
 
 class MobileLayout extends StatefulWidget {
   const MobileLayout({super.key});
@@ -26,6 +28,8 @@ class _MobileLayoutState extends State<MobileLayout> {
 
   @override
   Widget build(BuildContext context) {
+    int pageIndex = context.watch<AppProvider>().pageIndex;
+    if (_selectedIndex != pageIndex) setState(() => _selectedIndex = pageIndex);
     return Scaffold(
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: Container(
@@ -68,11 +72,10 @@ class _MobileLayoutState extends State<MobileLayout> {
               icon: Icon(Icons.settings), label: 'Settings'),
         ],
         onTap: (index) {
-          // Handle navigation here
-          setState(() => _selectedIndex = index);
+          context.read<AppProvider>().setPageIndex(index);
         },
       ),
-      body: pages[_selectedIndex],
+      body: pages[pageIndex],
     );
   }
 }

938fe2c27e54efdd1ce74cc894214915b6718c75
Eric Man <youzai0427@gmail.com>
2025-07-30 03:32:38 +0800

    add light theme color

--- lib/src/theme.dart
+++ lib/src/theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
+//TODO setup dark mode finish this up
 final myTheme = ThemeData(
     textTheme: TextTheme(
       bodyLarge: TextStyle(color: Colors.white),
@@ -19,7 +20,32 @@ final myTheme = ThemeData(
       unselectedItemColor: HexColor('#E9E6FD'),
       showUnselectedLabels: false,
     ),
+    primaryColor: Color(0xFFA291FF),
     colorScheme: ColorScheme.light().copyWith(
-        primary: HexColor('#504099'),
-        secondary: HexColor('#E9E6FD'),
-        surface: HexColor('#F9F8FD')));
+        primary: const Color(0xFF917EF3),
+        secondary: const Color(0xFF44339D),
+        surfaceBright: Color(0xFFD1CCFC),
+        surface: const Color(0xFFFAFAFA)));
+
+final darkTheme = ThemeData(
+    textTheme: TextTheme(
+      bodyLarge: TextStyle(color: Colors.white),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+        fillColor: Colors.orange,
+        filled: true,
+        hintStyle: TextStyle(color: Colors.grey),
+        labelStyle: TextStyle(color: Colors.black)),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: Colors.white,
+      selectedItemColor: HexColor('#504099'),
+      selectedLabelStyle: TextStyle(
+        fontWeight: FontWeight.w500,
+      ),
+      unselectedItemColor: HexColor('#E9E6FD'),
+      showUnselectedLabels: false,
+    ),
+    colorScheme: ColorScheme.light().copyWith(
+      primary: HexColor('#504099'),
+      secondary: HexColor('#E9E6FD'),
+    ));

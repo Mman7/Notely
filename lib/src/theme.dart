88a6e42551583e5ae2fc3b03b4136115ab9a0cde
Eric Man <youzai0427@gmail.com>
2025-08-19 04:23:28 +0800

    adjust color

--- lib/src/theme.dart
+++ lib/src/theme.dart
@@ -1,11 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
-//TODO setup dark mode finish this up
+//TODO set dark editor text color
+
 final myTheme = ThemeData(
     textTheme: TextTheme(
-      bodyLarge: TextStyle(color: Colors.white),
+        bodyLarge: TextStyle(color: Color(0xFF44339D)),
+        bodyMedium: TextStyle(color: Color(0xFF44339D)),
+        headlineSmall: TextStyle(color: Color(0xFF44339D)),
+        headlineLarge: TextStyle(color: Color(0xFF44339D)),
+        labelLarge: TextStyle(color: Colors.white70),
+        bodySmall: TextStyle(color: Color(0xFF44339D))),
+    inputDecorationTheme: InputDecorationTheme(
+        fillColor: Colors.orange,
+        filled: true,
+        hintStyle: TextStyle(color: Colors.grey),
+        labelStyle: TextStyle(color: Color(0xFF44339D))),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      selectedItemColor: HexColor('#504099'),
+      selectedLabelStyle: TextStyle(
+        fontWeight: FontWeight.w500,
+      ),
+      unselectedItemColor: HexColor('#E9E6FD'),
+      showUnselectedLabels: false,
     ),
+    primaryColor: Color(0xFFA291FF),
+    colorScheme: ColorScheme.light().copyWith(
+        tertiary: Color(0xFFFFFFFF),
+        primary: const Color.fromARGB(255, 163, 145, 255),
+        secondary: const Color(0xFF44339D),
+        surfaceBright: Color(0xFF44339D),
+        surface: const Color(0xFAFAFAFA)));
+
+final myDarkTheme = ThemeData(
+    textTheme: TextTheme(
+        bodyLarge: TextStyle(color: Colors.white),
+        headlineLarge: TextStyle(color: Colors.white),
+        headlineSmall: TextStyle(color: Colors.white),
+        bodyMedium: TextStyle(color: Colors.white),
+        labelLarge: TextStyle(color: Colors.grey),
+        bodySmall: TextStyle(color: Colors.white)),
     inputDecorationTheme: InputDecorationTheme(
         fillColor: Colors.orange,
         filled: true,
@@ -22,30 +56,8 @@ final myTheme = ThemeData(
     ),
     primaryColor: Color(0xFFA291FF),
     colorScheme: ColorScheme.light().copyWith(
-        primary: const Color(0xFF917EF3),
-        secondary: const Color(0xFF44339D),
+        primary: const Color(0xFF44339C),
+        secondary: const Color(0xFF0F0F41),
+        tertiary: Color(0xFF333333),
         surfaceBright: Color(0xFFD1CCFC),
-        surface: const Color(0xFFFAFAFA)));
-
-final darkTheme = ThemeData(
-    textTheme: TextTheme(
-      bodyLarge: TextStyle(color: Colors.white),
-    ),
-    inputDecorationTheme: InputDecorationTheme(
-        fillColor: Colors.orange,
-        filled: true,
-        hintStyle: TextStyle(color: Colors.grey),
-        labelStyle: TextStyle(color: Colors.black)),
-    bottomNavigationBarTheme: BottomNavigationBarThemeData(
-      backgroundColor: Colors.white,
-      selectedItemColor: HexColor('#504099'),
-      selectedLabelStyle: TextStyle(
-        fontWeight: FontWeight.w500,
-      ),
-      unselectedItemColor: HexColor('#E9E6FD'),
-      showUnselectedLabels: false,
-    ),
-    colorScheme: ColorScheme.light().copyWith(
-      primary: HexColor('#504099'),
-      secondary: HexColor('#E9E6FD'),
-    ));
+        surface: const Color(0xFF222222)));

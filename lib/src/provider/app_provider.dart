6e937dc4e723dc5a14733c2306522f05521592b2
Eric Man <youzai0427@gmail.com>
2025-08-19 04:23:41 +0800

    add app darkmode state

--- lib/src/provider/app_provider.dart
+++ lib/src/provider/app_provider.dart
@@ -3,15 +3,25 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:melonote/src/model/folder_model.dart';
 import 'package:melonote/src/model/note_model.dart';
 import 'package:melonote/src/modules/local_database.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AppProvider extends ChangeNotifier {
   List<Note> noteList = [];
   List<FolderModel> folderList = [];
   int pageIndex = 0;
   Database database = Database();
+  bool isDarkMode = false;
 
   setPageIndex(int index) {
     pageIndex = index;
+    notifyListeners();
+  }
+
+  darkModeToggle() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    isDarkMode = !isDarkMode;
+    prefs.setBool('darkmode', isDarkMode);
+    print(isDarkMode);
     notifyListeners();
   }
 
@@ -34,7 +44,10 @@ class AppProvider extends ChangeNotifier {
     folderList = result;
   }
 
-  intializeData() {
+  intializeData() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool? darkmode = prefs.getBool('darkmode') ?? false;
+    isDarkMode = darkmode;
     intializeNote();
     intializeFolder();
     notifyListeners();

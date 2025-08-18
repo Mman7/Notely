e1b15a880dae9bdf46d984b180cb82ec43bdf0b2
Eric Man <youzai0427@gmail.com>
2025-08-19 04:24:10 +0800

    add darkmode toggle

--- lib/src/section/settings_page.dart
+++ lib/src/section/settings_page.dart
@@ -13,7 +13,20 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   bool _isSwitch = false;
   @override
+  void initState() {
+    super.initState();
+    setState(() {
+      _isSwitch = context.read<AppProvider>().isDarkMode;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    toggle() {
+      setState(() => _isSwitch = !_isSwitch);
+      context.read<AppProvider>().darkModeToggle();
+    }
+
     DeviceType deviceType = context.read<AppProvider>().getDeviceType();
     return Scaffold(
       appBar: AppBar(
@@ -21,7 +34,7 @@ class _SettingsPageState extends State<SettingsPage> {
           'Settings',
           style: Theme.of(context).textTheme.headlineSmall?.copyWith(
                 fontSize: 20.sp,
-                color: Theme.of(context).colorScheme.secondary,
+                color: Theme.of(context).textTheme.bodyLarge?.color,
                 fontWeight: FontWeight.w600,
               ),
         ),
@@ -40,24 +53,19 @@ class _SettingsPageState extends State<SettingsPage> {
                 leading: Icon(
                   Icons.dark_mode,
                   size: 25.sp,
-                  color: Theme.of(context).colorScheme.secondary,
+                  color: Theme.of(context).colorScheme.surfaceBright,
                 ),
                 title: Text(
                   'Dark Mode',
                   style: TextStyle(
                     fontSize: 16.sp,
                     fontWeight: FontWeight.w600,
-                    color: Theme.of(context).colorScheme.secondary,
+                    color: Theme.of(context).colorScheme.surfaceBright,
                   ),
                 ),
-                trailing: Switch(
-                    value: _isSwitch,
-                    onChanged: (value) {
-                      setState(() => _isSwitch = !_isSwitch);
-                    }),
-                onTap: () {
-                  setState(() => _isSwitch = !_isSwitch);
-                },
+                trailing:
+                    Switch(value: _isSwitch, onChanged: (value) => toggle()),
+                onTap: () => toggle(),
               ),
             ],
           ),

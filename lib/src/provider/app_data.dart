a0dc19eb1cbca6804e0cfe5d37ddab1abdf330b9
Eric Man <youzai0427@gmail.com>
2025-09-19 02:22:12 +0800

    separate app data and status

---
+++ lib/src/provider/app_data.dart
@@ -0,0 +1,29 @@
+import 'package:notely/src/model/folder_model.dart';
+import 'package:notely/src/model/note_model.dart';
+import 'package:flutter/material.dart';
+import 'package:notely/src/modules/local_database.dart';
+
+class AppData extends ChangeNotifier {
+  List<Note> noteList = [];
+  List<FolderModel> folderList = [];
+
+  /// intialize or refresh notes
+  void intializeNote() {
+    noteList = Database.getAllNote();
+  }
+
+  /// intialize or refresh folders
+  void intializeFolder() {
+    folderList = Database.getAllFolder();
+  }
+
+  void intializeData() {
+    intializeNote();
+    intializeFolder();
+    notifyListeners();
+  }
+
+  void refreshAll() {
+    intializeData();
+  }
+}
--- lib/src/provider/app_status.dart
+++ lib/src/provider/app_status.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:notely/src/model/folder_model.dart';
-import 'package:notely/src/model/note_model.dart';
-import 'package:notely/src/modules/local_database.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class AppProvider extends ChangeNotifier {
-  List<Note> noteList = [];
-  List<FolderModel> folderList = [];
+class AppStatus extends ChangeNotifier {
   int pageIndex = 0;
-  Database database = Database();
   bool isDarkMode = false;
+  bool isSyncing = false;
+
+  toggleIsSyncing() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    isSyncing = !isSyncing;
+    prefs.setBool('issyncing', isSyncing);
+    notifyListeners();
+  }
 
   setPageIndex(int index) {
     pageIndex = index;
@@ -32,37 +34,12 @@ class AppProvider extends ChangeNotifier {
     return DeviceType.mobile;
   }
 
-  intializeNote() {
-    final note = database.getAllNote();
-    noteList = note;
-    notifyListeners();
-  }
-
-  intializeFolder() {
-    final result = database.getAllFolder();
-    folderList = result;
-  }
-
-  intializeData() async {
+  intializeStatus() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     bool? darkmode = prefs.getBool('darkmode') ?? false;
     isDarkMode = darkmode;
-    intializeNote();
-    intializeFolder();
-    notifyListeners();
-  }
-
-  refreshNote() {
-    intializeNote();
-  }
-
-  refreshFolder() {
-    intializeFolder();
-    notifyListeners();
-  }
-
-  refreshAll() {
-    intializeData();
+    bool? syncing = prefs.getBool('issyncing') ?? false;
+    isSyncing = syncing;
     notifyListeners();
   }
 }

13bfd704434c1867c40075bdb4f7921fe03f4394
Eric Man <youzai0427@gmail.com>
2025-09-19 02:35:47 +0800

    add deleted list functionality

--- lib/src/modules/local_database.dart
+++ lib/src/modules/local_database.dart
@@ -1,21 +1,24 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:notely/myobjectbox.dart';
 import 'package:notely/objectbox.g.dart';
 import 'package:notely/src/model/folder_model.dart';
 import 'package:notely/src/model/note_model.dart';
+import 'package:notely/src/modules/socket.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:uuid/uuid.dart';
 
 class Database {
-  final noteBox = objectbox.store.box<Note>();
-  final folderBox = objectbox.store.box<FolderModel>();
+  static final noteBox = objectbox.store.box<Note>();
+  static final folderBox = objectbox.store.box<FolderModel>();
 
-  void applyNotes(
+  static void applyDatas(
       {required List<Note> notesFromRemote,
       required List<FolderModel> remoteFolders}) {
     debugPrint('Applying data to database..');
     List<Note> localNotes = getAllNote();
     List<FolderModel> localFolders = getAllFolder();
-
     Set localNoteSet = localNotes.map((n) => n.uuid).toSet();
     Set localFoldersSet = localFolders.map((n) => n.uuid).toSet();
     // apply notes
@@ -54,37 +57,80 @@ class Database {
     }
   }
 
-  /// only for testing purpose
-  wipeAllData() {
-    noteBox.removeAll();
-    folderBox.removeAll();
+  static List<Note?> filterNoteByFolder({required List<int>? ids}) {
+    if (ids == null) return [];
+    return noteBox.getMany(ids);
   }
 
-  getNote({required int id}) {
-    final note = noteBox.get(id);
-    return note;
+  static getManyNote({required List<int> list}) {
+    return noteBox.getMany(list);
   }
 
-  getAllNote() {
+  static getAllNote() {
     final notes = noteBox.getAll();
     return notes;
   }
 
-  getAllFolder() {
+  static getAllFolder() {
     final noteBookList = objectbox.store.box<FolderModel>().getAll();
     return noteBookList;
   }
 
-  // Example: update an existing note by id
-  void updateNote({required Note note}) {
-    noteBox.put(note, mode: PutMode.update);
-  }
-
-  addNote({required Note note}) {
+  //  CRUD Operations for Note
+  static addNote({required Note note}) {
     noteBox.put(note);
+    SocketClient.connect();
   }
 
-  addFolder({
+  static getNote({required int id}) {
+    return noteBox.get(id);
+  }
+
+  // Example: update an existing note by id
+  static void updateNote({required Note note}) {
+    noteBox.put(note, mode: PutMode.update);
+    SocketClient.connect();
+  }
+
+  static removeNotesByUUIDs(List<String> ids) {
+    List<Note> notes = getAllNote();
+    List<int> notesToDelete = notes
+        .where((n) => ids.contains(n.uuid))
+        .toList()
+        .map((e) => e.id)
+        .toList();
+    noteBox.removeMany(notesToDelete);
+  }
+
+  static deleteNote({required Note note}) async {
+    noteBox.remove(note.id);
+    addDeleteList(note: note);
+    SocketClient.connect();
+  }
+
+  static addDeleteList({required Note note}) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final deleteList = prefs.getString('delete') ?? '[]';
+    dynamic decodedList = jsonDecode(deleteList);
+    decodedList.add(note.uuid);
+    prefs.setString('delete', jsonEncode(decodedList));
+  }
+
+  /// Returns a list of UUIDs for notes that have been marked as deleted.
+  static Future<List<String>> getDeletedNoteList() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final list = prefs.getString('delete') ?? '[]';
+    List<dynamic> decodedList = jsonDecode(list);
+    return decodedList.whereType<String>().toList();
+  }
+
+  static clearDeletedList() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove('delete');
+  }
+
+  //  CRUD Operations for Folder
+  static addFolder({
     required name,
   }) {
     FolderModel notebook = FolderModel(
@@ -93,36 +139,27 @@ class Database {
       title: name,
     );
     folderBox.put(notebook);
+    SocketClient.connect();
   }
 
-  deleteNote({required id}) {
-    noteBox.remove(id);
-  }
-
-  getManyNote({required List<int> list}) {
-    return noteBox.getMany(list);
-  }
-
-  clearAll() {
-    noteBox.removeAll();
-    folderBox.removeAll();
-  }
-
-  getFolder({required int id}) {
+  static getFolder({required int id}) {
     FolderModel? folder = folderBox.get(id);
     return folder;
   }
 
-  updateFolder({required FolderModel folder}) {
+  static updateFolder({required FolderModel folder}) {
     folderBox.put(folder, mode: PutMode.update);
+    SocketClient.connect();
   }
 
-  deleteFolder({required id}) {
+  static deleteFolder({required id}) {
     folderBox.remove(id);
+    SocketClient.connect();
   }
 
-  List<Note?> filterNoteByFolder({required List<int>? ids}) {
-    if (ids == null) return [];
-    return noteBox.getMany(ids);
+  /// only for testing purpose
+  static wipeAllData() {
+    noteBox.removeAll();
+    folderBox.removeAll();
   }
 }

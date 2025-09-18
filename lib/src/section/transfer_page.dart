69300990434870fac5a7224c7a35a32cc7d45bb3
Eric Man <youzai0427@gmail.com>
2025-09-19 02:53:38 +0800

    add received button waitting function

--- lib/src/section/transfer_page.dart
+++ lib/src/section/transfer_page.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:notely/src/modules/socket.dart';
-import 'package:notely/src/provider/app_provider.dart';
+import 'package:notely/src/provider/app_data.dart';
+import 'package:notely/src/provider/app_status.dart';
 import 'package:provider/provider.dart';
 import 'package:toastification/toastification.dart';
 
@@ -32,7 +33,7 @@ class _TransferPageState extends State<TransferPage> {
         // if dev set this to true the dialog will not close when click outside
         barrierDismissible: false,
         builder: (context) => AlertDialog(
-          backgroundColor: Theme.of(context).colorScheme.surface,
+          backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
           contentPadding: const EdgeInsets.all(24),
           content: Column(
             mainAxisSize: MainAxisSize.min,
@@ -44,7 +45,7 @@ class _TransferPageState extends State<TransferPage> {
               ),
               SizedBox(height: 16.h),
               Text(
-                'Waiting for connection...',
+                'Waiting for data...',
                 style: TextStyle(
                   overflow: TextOverflow.ellipsis,
                   fontSize: 16.sp,
@@ -61,7 +62,7 @@ class _TransferPageState extends State<TransferPage> {
                 setState(() {
                   isWaiting = false;
                 });
-                SocketServer().stopServer(completer);
+                SocketServer.stopServer(completer);
                 resetCompleter();
               },
               child: Text(
@@ -82,6 +83,9 @@ class _TransferPageState extends State<TransferPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        shape:
+            Border.all(color: Theme.of(context).colorScheme.surfaceContainer),
+        backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
         title: Text(
           'Transfer',
           style: TextStyle(
@@ -90,74 +94,94 @@ class _TransferPageState extends State<TransferPage> {
               color: Theme.of(context).textTheme.bodyLarge?.color),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Transfer Notes',
-              style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w600,
-                  color: Theme.of(context).textTheme.bodyLarge?.color),
-            ),
-            const SizedBox(height: 16),
-            btn(
-              icon: Icons.upload,
-              context: context,
-              text: "Send",
-              onpressed: () {
-                SocketClient().connect();
-              },
-            ),
-            const SizedBox(height: 16),
-            Divider(
-              color: Colors.grey.withAlpha(50),
-            ),
-            const SizedBox(height: 16),
-            btn(
-                icon: Icons.download,
-                context: context,
-                text: "Received",
-                // Host and waiting for data
-                onpressed: () async {
-                  setState(() {
-                    isWaiting = true;
-                  });
-                  SocketServer().startServer(() {
-                    Navigator.of(context).pop();
-                    setState(() {
-                      isWaiting = false;
-                    });
-                    completer.complete('Data received and applied');
-                    resetCompleter();
-                  });
-                  popupWaiting();
-                  final state = await completer.future;
-                  if (state == 'Data received and applied') {
-                    if (context.mounted) {
-                      context.read<AppProvider>().refreshAll();
-                      toastification.show(
-                          style: ToastificationStyle.minimal,
-                          primaryColor: Colors.lightGreen,
-                          icon: Icon(Icons.done),
-                          context:
-                              context, // optional if you use ToastificationWrapper
-                          title: Text('Your folder has been created'),
-                          pauseOnHover: false,
-                          autoCloseDuration: const Duration(seconds: 2));
-                    }
-                  }
-                }),
-            Expanded(child: Container()),
-            Center(
-              child: Text(
-                'The Windows device must allow this application through the firewall; otherwise, data transfer may fail',
-                style: TextStyle(color: Colors.grey.shade600),
+      body: Container(
+        decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.surfaceContainer,
+            border: Border.all(
+                color: Theme.of(context).colorScheme.surfaceContainer)),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Transfer Notes',
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                    color: Theme.of(context).textTheme.bodyLarge?.color),
               ),
-            )
-          ],
+              const SizedBox(height: 16),
+              btn(
+                icon: Icons.upload,
+                context: context,
+                text: "Send",
+                onpressed: () {
+                  SocketClient.connect();
+                },
+              ),
+              const SizedBox(height: 16),
+              Divider(
+                color: Colors.grey.withAlpha(50),
+              ),
+              const SizedBox(height: 16),
+              Tooltip(
+                message: context.read<AppStatus>().isSyncing
+                    ? "Automatic synchronization is enabled"
+                    : "Receive data",
+                child: Opacity(
+                  opacity: context.read<AppStatus>().isSyncing ? 0.5 : 1,
+                  child: IgnorePointer(
+                    ignoring: context.read<AppStatus>().isSyncing,
+                    child: btn(
+                        icon: Icons.download,
+                        context: context,
+                        text: "Received",
+                        // Host and waiting for data
+                        onpressed: () async {
+                          if (context.read<AppStatus>().isSyncing) return;
+                          setState(() {
+                            isWaiting = true;
+                          });
+                          SocketServer.startServer(callback: () {
+                            if (isWaiting) Navigator.of(context).pop();
+                            setState(() {
+                              isWaiting = false;
+                            });
+
+                            completer.complete('Data received and applied');
+                            resetCompleter();
+                          });
+                          popupWaiting();
+                          final state = await completer.future;
+                          if (state == 'Data received and applied') {
+                            if (context.mounted) {
+                              context.read<AppData>().refreshAll();
+                              toastification.show(
+                                  style: ToastificationStyle.minimal,
+                                  primaryColor: Colors.lightGreen,
+                                  icon: Icon(Icons.done),
+                                  context:
+                                      context, // optional if you use ToastificationWrapper
+                                  title: Text('Your folder has been created'),
+                                  pauseOnHover: false,
+                                  autoCloseDuration:
+                                      const Duration(seconds: 2));
+                            }
+                          }
+                        }),
+                  ),
+                ),
+              ),
+              Expanded(child: Container()),
+              Center(
+                child: Text(
+                  'The Windows device must allow this application through the firewall; otherwise, data transfer may fail',
+                  style: TextStyle(color: Colors.grey.shade600),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -191,13 +215,13 @@ class _TransferPageState extends State<TransferPage> {
           text,
           style: Theme.of(context).textTheme.headlineSmall?.copyWith(
                 fontSize: 20.sp,
-                color: Theme.of(context).colorScheme.surface,
+                color: Theme.of(context).colorScheme.surfaceContainer,
                 fontWeight: FontWeight.w600,
               ),
         ),
         icon: Icon(
           icon,
-          color: Theme.of(context).colorScheme.surface,
+          color: Theme.of(context).colorScheme.surfaceContainer,
           size: Theme.of(context).textTheme.headlineLarge?.fontSize,
         ),
       ),

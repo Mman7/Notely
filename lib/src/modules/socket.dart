6a36adc4897c26c00e818cbd0b4dbd0828d4b2a5
Eric Man <youzai0427@gmail.com>
2025-09-19 02:36:25 +0800

    add transfer data function

--- lib/src/modules/socket.dart
+++ lib/src/modules/socket.dart
@@ -3,11 +3,21 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:connectme/connectme.dart';
 import 'package:flutter/material.dart';
+import 'package:notely/main.dart';
 import 'package:notely/src/model/folder_model.dart';
 import 'package:notely/src/model/note_model.dart';
+import 'package:notely/src/modules/encrypt.dart';
 import 'package:notely/src/modules/local_database.dart';
 import 'package:network_info_plus/network_info_plus.dart';
 import 'package:notely/src/modules/web_socket.dart';
+import 'package:notely/src/provider/app_data.dart';
+import 'package:provider/provider.dart';
+
+import 'package:uuid/uuid.dart';
+
+String randomId = Uuid().v4();
+bool isHosting = false;
+
 // MAIN SOCKET
 
 // Phone → UDP broadcast → “Hello, server?”
@@ -17,33 +27,27 @@ import 'package:notely/src/modules/web_socket.dart';
 // Phone → TCP connect → 192.168.1.5:4040
 
 class SocketClient {
-  connectWithTcp({serverIp, tcpPort}) {
+  static final context = navigatorKey.currentContext as BuildContext;
+  static _connectWithTcp({serverIp, tcpPort}) {
     WebSocketClient(serverAddress: '$serverIp:$tcpPort')
         .connect()
-        .then((client) {
-      debugPrint(
-          'from Cli Connected to WebSocket server at $serverIp:$tcpPort');
-
-      List<FolderModel> folders = Database().getAllFolder();
-      List<Note> notes = Database().getAllNote();
-      dynamic data = jsonEncode({
-        'type': 'sync',
-        'folders': folders,
-        'notes': notes,
+        .then((client) async {
+      client.send(AES256GCM.encrypt(plaintext: await getLocalData()));
+      client.listen<String>((data) {
+        // client.close();
+        handleData(data, context);
+        Database.clearDeletedList();
       });
-      client.send(data);
     });
   }
 
-  void connect({required Completer connector}) async {
+  static void connect() async {
     final udpSocket = await RawDatagramSocket.bind(InternetAddress.anyIPv4, 0);
     udpSocket.broadcastEnabled = true;
     final broadcastAddress = await getBroadcastAddress();
     final broadcastPort = 8888;
-
     udpSocket.send(
         'DISCOVER_SERVER'.codeUnits, broadcastAddress, broadcastPort);
-    debugPrint('Broadcast message sent');
 
     udpSocket.listen((event) async {
       if (event == RawSocketEvent.read) {
@@ -51,26 +55,34 @@ class SocketClient {
         if (datagram == null) return;
         final message = String.fromCharCodes(datagram.data);
         debugPrint(
-            'from Cli Received: $message from ${datagram.address.address}:${datagram.port}');
+            'Received UDP message: $message from ${datagram.address}:${datagram.port}');
         if (message.startsWith('SERVER_HERE')) {
           final parts = message.split(':');
           final tcpPort = int.parse(parts[1]);
           final serverIp = datagram.address.address;
-          connectWithTcp(serverIp: serverIp, tcpPort: tcpPort);
-          debugPrint('from Cli Server found at $serverIp:$tcpPort');
-          udpSocket.close();
+          _connectWithTcp(serverIp: serverIp, tcpPort: tcpPort);
         }
       }
     });
   }
 }
 
-handleData(data) {
-  var decodedData = jsonDecode(data);
+// Handle incoming data
+void handleData(data, BuildContext context) async {
+  debugPrint('data received');
+  dynamic decodedData = jsonDecode(AES256GCM.decrypt(base64Cipher: data));
+  dynamic foldersAndNotes = decodedData['data'];
+  List<String> deletedList = List<String>.from(decodedData['deletedList']);
+
+  if (decodedData['uid'] == randomId) {
+    debugPrint('Ignoring own sync data');
+    return;
+  }
+
   if (decodedData['type'] == 'sync') {
     List<Note> notes = [];
-    for (var i = 0; i <= decodedData['notes'].length - 1; i++) {
-      dynamic noteFromData = decodedData['notes'][i];
+    for (var i = 0; i <= foldersAndNotes['notes'].length - 1; i++) {
+      dynamic noteFromData = foldersAndNotes['notes'][i];
       Note note = Note(
           content: noteFromData['content'],
           title: noteFromData['title'],
@@ -83,61 +95,93 @@ handleData(data) {
       notes.add(note);
     }
     List<FolderModel> folders = [];
-    for (var i = 0; i <= decodedData['folders'].length - 1; i++) {
-      dynamic folderFromData = decodedData['folders'][i];
+    for (var i = 0; i <= foldersAndNotes['folders'].length - 1; i++) {
+      dynamic folderFromData = foldersAndNotes['folders'][i];
       FolderModel folder = FolderModel(
-          uuid: folderFromData['uuid'],
-          title: folderFromData['title'],
-          noteInclude: folderFromData['noteInclude']);
+        uuid: folderFromData['uuid'],
+        title: folderFromData['title'],
+        noteInclude: folderFromData['noteInclude'],
+      );
       folders.add(folder);
     }
-    Database().applyNotes(notesFromRemote: notes, remoteFolders: folders);
+    Database.applyDatas(notesFromRemote: notes, remoteFolders: folders);
+    Database.removeNotesByUUIDs(deletedList);
+    context.read<AppData>().refreshAll();
   }
 }
 
-class SocketServer {
-  String clientIp = '';
-  ConnectMeServer? server;
+Future<String> getLocalData() async {
+  List<FolderModel> folders = Database.getAllFolder();
+  List<Note> notes = Database.getAllNote();
+  List<String> deletedList = await Database.getDeletedNoteList();
 
-  stopServer(Completer completer) async {
-    await server?.close();
+  String data = jsonEncode({
+    'type': 'sync',
+    'uid': randomId,
+    'deletedList': deletedList,
+    'data': {'folders': folders, 'notes': notes},
+  });
+
+  return data;
+}
+
+class SocketServer {
+  static late ConnectMeServer wsServer;
+  static late RawDatagramSocket udpServer;
+
+  static stopServer(
+    Completer completer,
+  ) async {
+    await wsServer.close();
+    stopUDPServer();
+    isHosting = false;
     completer.complete('Server stopped by user');
     debugPrint('Server stopped');
   }
 
-  startServer(callback) async {
-    await WebSocketServer(clientIp: clientIp).startServer().then((server) {
-      server = server;
-      server.listen<String>((data, client) async {
-        handleData(data);
-        callback();
-        await client.close();
-        await server.close();
-      });
-    });
+  static stopUDPServer() {
+    udpServer.close();
+  }
 
+  // Start UDP server to listen for discovery messages and TCP server for connections
+  static startServer({callback}) async {
     /// UDP
+    if (isHosting) return;
     final udpSocket =
         await RawDatagramSocket.bind(InternetAddress.anyIPv4, 8888);
     debugPrint('UDP server listening on port 8888');
+    udpServer = udpSocket;
     udpSocket.listen((event) {
       if (event == RawSocketEvent.read) {
         final datagram = udpSocket.receive();
+        // Ignore if packet comes from this device
         if (datagram == null) return;
-
         final message = String.fromCharCodes(datagram.data);
-        clientIp = datagram.address.address;
-        debugPrint(
-            'from Serv Received: $message from ${datagram.address.address}:${datagram.port}');
 
         if (message == 'DISCOVER_SERVER') {
           final response = 'SERVER_HERE:4040'; // include TCP port
           udpSocket.send(response.codeUnits, datagram.address, datagram.port);
-
-          debugPrint(
-              'from Serv Sent response to ${datagram.address.address}:${datagram.port}');
         }
       }
+    });
+    // WS
+    _startTCPserver(
+      callback: callback,
+    );
+  }
+
+  static _startTCPserver({callback}) async {
+    final context = navigatorKey.currentContext as BuildContext;
+    await WebSocketServer().startServer().then((server) {
+      wsServer = server;
+      isHosting = true;
+      server.listen<String>((data, client) async {
+        handleData(data, context);
+        client.send(AES256GCM.encrypt(plaintext: await getLocalData()));
+        if (callback != null) callback();
+        // client.close();
+        // server.close();
+      });
     });
   }
 }
@@ -156,7 +200,6 @@ Future<InternetAddress> getBroadcastAddress() async {
   final broadcastParts =
       List.generate(4, (i) => ipParts[i] | (~maskParts[i] & 0xFF));
   final broadcastIp = broadcastParts.join('.');
-  debugPrint('Calculated broadcast address: $broadcastIp');
 
   return InternetAddress(broadcastIp);
 }

a8277da1ac3c7fa026c9debc56e1b6fdb8221b11
Eric Man <youzai0427@gmail.com>
2025-09-19 02:37:29 +0800

    remove debugPrint

--- lib/src/modules/web_socket.dart
+++ lib/src/modules/web_socket.dart
@@ -3,23 +3,24 @@ import 'package:connectme/connectme.dart';
 import 'package:flutter/material.dart';
 
 class WebSocketServer {
-  WebSocketServer({required this.clientIp});
-  String clientIp;
   Future<ConnectMeServer> startServer() async {
-    final ConnectMeServer server = await ConnectMe.serve(
-      InternetAddress.anyIPv4,
-      port: 4040,
-      onConnect: (ConnectMeClient client) {
-        debugPrint('$clientIp connected.');
-      },
-      onDisconnect: (ConnectMeClient client) {
-        debugPrint('$clientIp disconnected.');
-      },
-      onLog: (p0) => debugPrint(p0),
-      type: ConnectMeType
-          .ws, // by default, means using WebSocket server, can be also pure TCP
-    );
-    return server;
+    try {
+      final ConnectMeServer server = await ConnectMe.serve(
+        InternetAddress.anyIPv4,
+        port: 4040,
+        onConnect: (ConnectMeClient client) {
+          debugPrint('someone connected WS.');
+        },
+        onDisconnect: (ConnectMeClient client) {
+          debugPrint('someone disconnected from WS.');
+        },
+        onLog: (p0) => debugPrint(p0),
+        type: ConnectMeType.ws,
+      );
+      return server;
+    } catch (e) {
+      throw Error();
+    }
   }
 }
 
@@ -30,10 +31,7 @@ class WebSocketClient {
   Future<ConnectMeClient> connect() async {
     final ConnectMeClient client =
         await ConnectMe.connect('ws://$serverAddress');
-    // Listen for reverse String messages from the server.
-    client.listen<String>((String message) {
-      debugPrint('Client received: $message');
-    });
+    // await ConnectMe.connect('$serverAddress');
     return client;
   }
 }
